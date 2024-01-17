e4fa067e6aefebf5aa24663125bb2cf73b082786
adamusek-007 <adam.kuczabinski@gmail.com>
2024-01-17 08:27:14 +0100

    Add commands for restarting programms in c++

--- start.bat
+++ start.bat
@@ -5,3 +5,12 @@ docker build -t cpp_client -f Dockerfile.client .
 docker run --name server cpp_server
 
 docker run --name client --link server:server cpp_client
+
+
+
+
+
+docker compose server exec g++ -o server server.cpp -lzmq \ ./server
+
+
+docker compose client exec g++ -o client client.cpp -lzmq \ ./client
