2b83c85c495ccc4ac417c49bc059c3c733af4470
adamusek-007 <adam.kuczabinski@gmail.com>
2024-02-05 09:10:45 +0100

    Use not deprecated declarations, add if stmt

--- server.cpp
+++ server.cpp
@@ -10,15 +10,20 @@ int main()
     while (true)
     {
         zmq::message_t request;
-        socket.recv(&request);
+        zmq::recv_result_t result = socket.recv(request, zmq::recv_flags::none);
+
+        if (!result.has_value())
+        {
+            std::cerr << "Error receiving message" << std::endl;
+            continue; // Handle the error as needed
+        }
 
         std::cout << "Received request: " << static_cast<char *>(request.data()) << std::endl;
 
         zmq::message_t reply(15);
         memcpy(reply.data(), "Hello, client!", 15);
-        socket.send(reply);
+        socket.send(reply, zmq::send_flags::none);
     }
 
     return 0;
 }
- 
