52f2a696ec4f267dd56d8634dad17ef83f3cc191
adamusek-007 <adam.kuczabinski@gmail.com>
2024-02-05 09:14:08 +0100

    Complete rewrite server in cpp

--- server.cpp
+++ server.cpp
@@ -1,29 +1,22 @@
 #include <zmq.hpp>
 #include <iostream>
+#include <string>
 
 int main()
 {
     zmq::context_t context(1);
-    zmq::socket_t socket(context, ZMQ_REP);
-    socket.bind("tcp://*:5555");
+    zmq::socket_t socket(context, ZMQ_REQ);
+    socket.connect("tcp://192.168.100.5:5555");
 
-    while (true)
-    {
-        zmq::message_t request;
-        zmq::recv_result_t result = socket.recv(request, zmq::recv_flags::none);
+    const std::string request_data = "Hello client there!";
+    zmq::message_t request(request_data.size());
+    memcpy(request.data(), request_data.c_str(), request_data.size());
+    socket.send(request, zmq::send_flags::none);
 
-        if (!result.has_value())
-        {
-            std::cerr << "Error receiving message" << std::endl;
-            continue; // Handle the error as needed
-        }
+    zmq::message_t reply;
+    socket.recv(reply, zmq::recv_flags::none);
 
-        std::cout << "Received request: " << static_cast<char *>(request.data()) << std::endl;
-
-        zmq::message_t reply(15);
-        memcpy(reply.data(), "Hello, client!", 15);
-        socket.send(reply, zmq::send_flags::none);
-    }
+    std::cout << "Received reply: " << std::string(static_cast<char *>(reply.data()), reply.size()) << std::endl;
 
     return 0;
 }
