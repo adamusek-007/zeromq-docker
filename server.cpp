76e6e62a39c40cedb760152447179e703270d4ee
adamusek-007 <adam.kuczabinski@gmail.com>
2024-02-05 09:15:02 +0100

    Add conditions to code

--- server.cpp
+++ server.cpp
@@ -4,19 +4,31 @@
 
 int main()
 {
-    zmq::context_t context(1);
-    zmq::socket_t socket(context, ZMQ_REQ);
-    socket.connect("tcp://192.168.100.5:5555");
+    try
+    {
+        zmq::context_t context(1);
+        zmq::socket_t socket(context, ZMQ_REQ);
+        socket.connect("tcp://192.168.100.5:5555");
 
-    const std::string request_data = "Hello client there!";
-    zmq::message_t request(request_data.size());
-    memcpy(request.data(), request_data.c_str(), request_data.size());
-    socket.send(request, zmq::send_flags::none);
+        const std::string request_data = "Hello client there!";
+        zmq::message_t request(request_data.size());
+        memcpy(request.data(), request_data.c_str(), request_data.size());
+        socket.send(request, zmq::send_flags::none);
 
-    zmq::message_t reply;
-    socket.recv(reply, zmq::recv_flags::none);
-
-    std::cout << "Received reply: " << std::string(static_cast<char *>(reply.data()), reply.size()) << std::endl;
+        zmq::message_t reply;
+        if (socket.recv(reply, zmq::recv_flags::none))
+        {
+            std::cout << "Received reply: " << std::string(static_cast<char *>(reply.data()), reply.size()) << std::endl;
+        }
+        else
+        {
+            std::cerr << "Error receiving reply." << std::endl;
+        }
+    }
+    catch (const zmq::error_t &e)
+    {
+        std::cerr << "ZMQ Error: " << e.what() << std::endl;
+    }
 
     return 0;
 }
