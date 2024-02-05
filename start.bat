95df80fee05c5edfe76d7df21c790f584df19871
adamusek-007 <adam.kuczabinski@gmail.com>
2024-02-05 09:13:41 +0100

    Add sudo for easier copying in start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
-docker build --tag cpp_server --file Dockerfile.server .
-docker build --tag cpp_client --file Dockerfile.client .
-docker run --name server cpp_server
-docker run --name client --link server:server --rm cpp_client
+sudo docker build --tag cpp_server --file Dockerfile.server .
+sudo docker build --tag cpp_client --file Dockerfile.client .
+sudo docker run --name server cpp_server
+sudo docker run --name client --link server:server --rm cpp_client
 
 @REM docker compose server exec g++ -o server server.cpp -lzmq \ ./server
 @REM docker compose client exec g++ -o client client.cpp -lzmq \ ./client
